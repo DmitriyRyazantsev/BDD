7af35a86df95d59098892e903a283ffa4311195c
Dmitry <dmitry.ryazantsev@mail.ru>
2023-08-01 17:29:57 +0300

    first commit

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -8,26 +8,45 @@ public class DataHelper {
     private DataHelper() {
     }
 
-    public static VereficationCode getVereficationCode() {
-        return new VereficationCode("12345");
+    public static VerificationCode getVerificationCode() {
+        return new VerificationCode("12345");
     }
 
     public static AutoInfo getAutoInfo() {
-        return new AutoInfo("vasya","qwerty123");
+        return new AutoInfo("vasya", "qwerty123");
     }
 
-    public static  CardInfo getFirstCardInfo() {
-        return new CardInfo("5559 0000 0000 0001");
+    public static CardInfo getFirstCardInfo() {
+        return new CardInfo("5559 0000 0000 0001", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
     }
-    public static  CardInfo getSecondCardInfo() {
-        return new CardInfo("5559 0000 0000 0002");
+
+    public static CardInfo getSecondCardInfo() {
+        return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
     }
+
     public static int generateValidAmount(int balance) {
-        return new Random().nextInt(balance)+1;
+        return new Random().nextInt(balance) + 1;
     }
+
     public static int generateInvalidAmount(int balance) {
         return Math.abs(balance) + new Random().nextInt(10000);
     }
-    @Value
 
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
+
+    @Value
+    public static class AutoInfo {
+        String login;
+        String password;
+    }
+
+
+    @Value
+    public static class CardInfo {
+        String cardNumber;
+        String testId;
+    }
 }
